45df7b4a6ddcdd3f3be4c05cd0db0eb53d281dde
drmth <79118141+drmth@users.noreply.github.com>
2022-07-07 01:37:39 +0200

    Challenge 5 with light management

--- Bicycle.php
+++ Bicycle.php
@@ -2,7 +2,21 @@
 
 require_once 'Vehicle.php';
 
-class Bicycle extends Vehicle
+class Bicycle extends Vehicle implements LightableInterface
 {
+
+  public function switchOn(): bool
+  {
+    if ($this->currentSpeed > 10) {
+      return true;
+    } else {
+      return false;
+    }
+  }
   
+  public function switchOff(): bool
+  {
+    return false;
+  }
+
 }
